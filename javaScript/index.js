2cbf7e4e0cce936246f0c9c2829133d2fbdc429b
“noxar94” <Lundmark1223@hotmail.se>
2023-05-11 10:29:05 +0200

    Update index.js
    switch for menu and choices

--- javaScript/index.js
+++ javaScript/index.js
@@ -23,4 +23,19 @@ function menu() {
   4. Get account name
   5. Exit
   `;
+
+  switch (choice) {
+    case "1":
+      break;
+    case "2":
+      break;
+    case "3":
+      break;
+    case "4":
+      break;
+    case "5":
+      prompt("exited");
+      break;
+    default:
+  }
 }
