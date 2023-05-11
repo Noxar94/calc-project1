7f8499344928192bb25ace99f24f723966c2f7d5
“noxar94” <Lundmark1223@hotmail.se>
2023-05-11 10:26:49 +0200

    Update index.js
    addedf menu

--- javaScript/index.js
+++ javaScript/index.js
@@ -11,3 +11,16 @@ const accountObj = {
 
   getAccountName: function (amount) {},
 };
+
+function menu() {
+  let choice;
+
+  const menu = `
+  Select a choice:
+  1. Get balance
+  2. Deposit
+  3. Withdrawal
+  4. Get account name
+  5. Exit
+  `;
+}
